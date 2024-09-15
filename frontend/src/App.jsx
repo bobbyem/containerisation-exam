b5b99720546000c89ec0f66049608744518bb94a
Bobby Rösehag <bobbyrosehag@gmail.com>
2024-09-15 18:11:28 +0200

    Fixed the docker-files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import {useState} from "react"
-import { TestButton } from './components/TestButton/TestButton'
 import { TodontForm } from './components/TodontForm';
 
 function App() {
-  const [testResponse, setTestResponse] = useState(null);
-
   return (
     <>
       <TodontForm/>
-      <h1>{testResponse}</h1>
     </>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Gör servern tillgänglig på alla nätverksgränssnitt
+    port: 5173
+  }
 })
