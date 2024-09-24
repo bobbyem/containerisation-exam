db86dce384c61809c46a0e61ba41f6e5f903d2b4
bobbyem <bobbyem@gmail.com>
2024-09-24 13:55:28 +0200

    Added retry to connect

--- backend/consumer/consumer.js
+++ backend/consumer/consumer.js
@@ -1,9 +1,27 @@
 const amqp = require("amqplib");
 const Todont = require("../models/models");
 
+async function connectWithRetry() {
+  let retries = 5;
+  while (retries) {
+    try {
+      const connection = await amqp.connect(
+        "amqp://user:password@rabbitmq:5672"
+      );
+      console.log("Connected to RabbitMQ");
+      return connection;
+    } catch (error) {
+      console.error("Failed to connect to RabbitMQ, retrying...", error);
+      retries -= 1;
+      await new Promise((res) => setTimeout(res, 5000)); // Wait 5 seconds before retrying
+    }
+  }
+  throw new Error("Could not connect to RabbitMQ");
+}
+
 async function consume() {
   try {
-    const connection = await amqp.connect("amqp://user:password@rabbitmq:5672");
+    const connection = await connectWithRetry();
     const channel = await connection.createChannel();
 
     const queue = "todont_queue";
@@ -12,7 +30,7 @@ async function consume() {
 
     channel.consume(queue, async (msg) => {
       const todont = JSON.parse(msg.content.toString());
-      console.log("Recieved todont", todont);
+      console.log("Received todont", todont);
 
       const newTodont = new Todont({
         title: todont.title,
