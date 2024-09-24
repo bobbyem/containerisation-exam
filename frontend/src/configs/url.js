d45054698b854842247ccf0526b009e8900b4cb3
bobbyem <bobbyem@gmail.com>
2024-09-24 14:53:52 +0200

    Debug Frontend

--- frontend/src/configs/url.js
+++ frontend/src/configs/url.js
@@ -1,3 +1,1 @@
-const PORT = process.env.REACT_APP_BACKEND_PORT || 3000;
-export const URL =
-  `${process.env.REACT_APP_HOST_URL}:${PORT}` || "http://localhost:3000";
+export const URL = "http://13.60.62.126:3000";
