3a76b335fa7eea1db4f811d10edc93564c63f30f
bobbyem <bobbyem@gmail.com>
2024-09-24 14:59:31 +0200

    Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Gör servern tillgänglig på alla nätverksgränssnitt
-    port: 80,
+    port: 5173,
     watch: {
       usePolling: true, // För att stödja live reload i Docker
     },
